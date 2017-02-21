17e6dbeb6f5e9e42fbef44f76ab4d03131c0c1f0
kilikamars <zeiger@hotmail.fr>
2017-02-21 15:41:58 +0100

    Premier jet

--- ListePersonne.h
+++ ListePersonne.h
@@ -1,26 +1,31 @@
 #ifndef LISTEPERSONNE_H
 #define LISTEPERSONNE_H
+#include<string>
+#include<iostream>
 
+using namespace std;
 struct Personne{
-  char prenom;
-  char nom;
-  int tel;
-  char mail;
+  string prenom,nom,tel,mail;
+  Personne *suiv;
 };
 
 class ListePersonne
 {
     public:
         ListePersonne();
-
-        ListePersonne(char prenom,char nom, int tel,char mail);
-        void ajouter();
-        void supprimer();
-        void modifier();
-    
-    protected:
-
+        ~ListePersonne();
+  //    ListePersonne(Personne &p); vraiment utile de faire ce constructeur ?
+/*      void ajouter(string prenom, string nom,string tel, string mail);
+        void supprimer(string prenom, string nom,string tel, string mail);
+        void modifier(string prenom, string nom,string tel, string mail);
+        bool estDouble(string prenom, string nom,string tel, string mail);
+*/
+        void ajouter(Personne *pe);
+        void supprimer(Personne *pe);
+        void modifier(Personne *pe);
+        bool estDouble(Personne *pe);
     private:
+    	Personne *p;
 };
 
 #endif // LISTEPERSONNE_H
