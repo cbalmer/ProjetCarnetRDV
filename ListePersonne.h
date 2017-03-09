41572ee2a27e9e89d2f13c9806a62a564c33798b
kilikamars <zeiger@hotmail.fr>
2017-03-09 17:52:05 +0100

    MAJ 9/3/2017

--- ListePersonne.h
+++ ListePersonne.h
@@ -2,9 +2,11 @@
 #define LISTEPERSONNE_H
 #include<string>
 #include<iostream>
+#include "ListeRDV.h"
+
 
 struct Personne{
-  string prenom,nom,tel,mail;
+  std::string prenom,nom,tel,mail;
   Personne *suiv;
 };
 
@@ -13,14 +15,18 @@ class ListePersonne
     public:
         ListePersonne();
         ~ListePersonne();
-
-
-        void ajouter(Personne *pe); //Permet l'ajout d'une personne
+  //    ListePersonne(Personne &p); vraiment utile de faire ce constructeur ?
+      void ajouter(std::string prenom, std::string nom,std::string tel, std::string mail);
+        void supprimer(std::string prenom, std::string nom,std::string tel, std::string mail);
+        void modifier(std::string prenom, std::string nom,std::string tel, std::string mail);
+        bool estDouble(std::string prenom, std::string nom,std::string tel, std::string mail);
+/*
+        void ajouter(Personne *pe); //Permet l'ajout d'une personne*/
         void supprimer(Personne *pe); //Permet la suppression d'une personne
         void modifier(Personne *pe);// Permet la modification d'une personne
         bool estDouble(Personne *pe);// permet de voir si une personne est déjà en contact
         bool estRDV(Personne *pe,RDV *rd);// permet de savoir si une personne est dans un rendez-vous
-  
+
     private:
     	Personne *p;
 };
