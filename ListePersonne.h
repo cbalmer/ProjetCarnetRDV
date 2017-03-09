e4604ed726ccdf5b088d370f25385fb220cb31bb
kilikamars <zeiger@hotmail.fr>
2017-03-09 16:06:25 +0100

    Update ListePersonne.h

--- ListePersonne.h
+++ ListePersonne.h
@@ -20,10 +20,12 @@ class ListePersonne
         void modifier(string prenom, string nom,string tel, string mail);
         bool estDouble(string prenom, string nom,string tel, string mail);
 */
-        void ajouter(Personne *pe);
-        void supprimer(Personne *pe);
-        void modifier(Personne *pe);
-        bool estDouble(Personne *pe);
+        void ajouter(Personne *pe); //Permet l'ajout d'une personne
+        void supprimer(Personne *pe); //Permet la suppression d'une personne
+        void modifier(Personne *pe);// Permet la modification d'une personne
+        bool estDouble(Personne *pe);// permet de voir si une personne est déjà en contact
+        bool estRDV(Personne *pe,RDV *rd);// permet de savoir si une personne est dans un rendez-vous
+  
     private:
     	Personne *p;
 };
