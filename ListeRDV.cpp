7625a1aa32acd9e8d8b9bca9629703946d76f11f
Camtouille <henzelin.camille@gmail.com>
2017-03-09 18:24:32 +0100

    Add files via upload

--- ListeRDV.cpp
+++ ListeRDV.cpp
@@ -1,6 +1,69 @@
 #include "ListeRDV.h"
 
-ListeRDV::ListeRDV()
+ListeRDV::ListeRDV():_tete{nullptr}
+{}
+
+ListeRDV::~ListeRDV()
 {
-    //ctor
+    while(_tete!=0)
+    {
+        RDV *c=_tete->suiv;
+        delete _tete;
+        _tete=c;
+    }
+}
+
+void ListeRDV::ajouter( const std::string & nom, const Date & d, const Horaire & hDeb, const Horaire & hFin)
+{
+    RDV *c = _tete;
+    RDV *nvRDV = new RDV{nom, d, hDeb, hFin};
+    if(_tete != nullptr)
+    {
+        while(c->suiv != nullptr)
+        {
+            c = c->suiv;
+        }
+        c->suiv = nvRDV;
+    }
+    else
+    {
+        _tete = nvRDV;
+    }
+    nvRDV->suiv = nullptr;
+}
+
+RDV* ListeRDV::recherche(const std::string & nom)
+{
+    RDV* c = _tete;
+    if(_tete == nullptr)
+    {
+        return nullptr;
+    }
+    else
+    {
+        if(_tete->Nom == nom)
+        {
+            return _tete;
+        }
+        else
+        {
+            while(c->suiv != nullptr && c->suiv->Nom != nom)
+            {
+                c = c->suiv;
+            }
+            if(c->suiv == nullptr)
+            {
+                return nullptr;
+            }
+            else
+            {
+                return c->suiv;
+            }
+        }
+    }
+}
+
+void ListeRDV::supprimer(const std::string & nom)
+{
+
 }
--- ListeRDV.h
+++ ListeRDV.h
@@ -1,27 +1,30 @@
 #ifndef LISTERDV_H
 #define LISTERDV_H
+#include "Date.h"
+#include "Horaire.h"
 
 struct RDV{
-    int jour;
-    int mois;
-    int annee;
-    int heureDebut;
-    int heureFin;
+    std::string Nom;
+    Date d;
+    Horaire hDeb;
+    Horaire hFin;
+    RDV *suiv;
 };
 
 class ListeRDV
 {
     public:
         ListeRDV();
-        
-        ListeRDV(int jour,int mois, int annee,int heureDebut,int heureFin);
-        void ajouter();
-        void supprimer();
-        void modifier();
-    
-    protected:
+        ~ListeRDV();
+        void ajouter(const std::string & nom, const Date & d, const Horaire & hDeb, const Horaire & hFin);
+        RDV* recherche(const std::string & nom);
+        void supprimer(const std::string & nom);
+        void modifierDate(const std::string & nom, const Date & d);
+        void modifierHeureDebut(const std::string & nom, const Horaire & hDeb);
+        void modifierHeureFin(const std::string & nom, const Horaire & hFin);
 
     private:
+        RDV *_tete;
 };
 
 #endif // LISTERDV_H
