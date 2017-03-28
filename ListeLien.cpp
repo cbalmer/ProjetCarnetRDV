c93682fec21f7c737a69aa19d5c3e99d39b9d193
CamCamStudio <cbalmer@student.42.fr>
2017-03-28 21:53:05 +0200

    Update ListeLien.cpp

--- ListeLien.cpp
+++ ListeLien.cpp
@@ -1,3 +1,23 @@
 #include "ListeLien.h"
 
 ListeLien::ListeLien()
+
+ bool ListeLien::ajouter(Personne *p, RDV *r)
+{
+    Lien *lelien= new Lien;
+    lelien->p=p;
+    lelien->r=r;
+    if(_tete==0)
+    {
+        _lien=lelien;
+    }
+    else
+    {
+        Lien *curs=_tete;
+        while(curs->suiv!=0)
+        {
+            curs=curs->suiv;
+        }
+        curs->suiv=lelien;
+    }
+}
