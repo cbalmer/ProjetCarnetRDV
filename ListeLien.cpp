c0b3a9635a927e16b751f952ddcc34efddcecc08
kilikamars <zeiger@hotmail.fr>
2017-03-28 23:07:20 +0200

    Update ListeLien.cpp

--- ListeLien.cpp
+++ ListeLien.cpp
@@ -52,6 +52,39 @@ bool supprimer(Personne* p, RDV* r)
 	return true;
 }
 
+bool ListeLien::supprimer(RDV* r)
+{
+if(_tete)
+	{
+		if( _tete->r == r)
+		{
+			Lien* s = _tete;
+			_tete = _tete->suiv;
+			delete s;
+		}
+		else
+		{
+			Lien* c = _tete;
+			while(c->suiv && ( c->suiv->r != r))
+			{
+				c = c->suiv;
+			}
+
+			if(c->suiv)//si c->suiv est différent de null c'est que c'est le lien à supprimer
+			{
+				Lien* s = c->suiv;
+				c->suiv = c->suiv->suiv;
+				delete s;
+			}
+			else
+				return false;//pas de correspondance
+		}
+	}
+	else
+		return false;
+
+	return true;
+}
 
 bool ListeLien::ajouter(Personne *p, RDV *r)
 {
