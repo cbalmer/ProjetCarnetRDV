6bdaa15b710208d8e62ee110f9a54d8499b91107
kilikamars <zeiger@hotmail.fr>
2017-03-28 23:45:52 +0200

    Update CarnetRDV.h

--- CarnetRDV.h
+++ CarnetRDV.h
@@ -13,7 +13,7 @@ class CarnetRDV
 
 		bool creerRDV(std::string const& nom, std::string const& prenom, Date const& date, Horaire const& hDeb);
 		bool creerRDV(std::string const &nom, std :: string const &prenom , Date const &date,Horaire const & hDeb,Horaire const  &hFin);
-		bool supprinerRDV(std::string const &nom );
+		bool supprimerRDV(std::string const &nom );
 		bool ajouterPersonneRDV(std::string const  &nomRDV, std::string const &NomPersonne, const std::string &PrenomPersonne);
 		bool supprimerPersonneRDV(std::string const  &nomRDV, std::string const &NomPersonne, const std::string &PrenomPersonne);
 		bool ajouterPersonne(std::string &prenom, std::string &nom,std::string  &tel, std::string &mail);
