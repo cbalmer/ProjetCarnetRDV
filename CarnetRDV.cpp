bc0584d2752dd67cbd0d9b5cc1df05b384abfab8
kilikamars <zeiger@hotmail.fr>
2017-03-28 20:59:53 +0200

    Add files via upload

--- CarnetRDV.cpp
+++ CarnetRDV.cpp
@@ -1,26 +1,8 @@
 #include "CarnetRDV.h"
 
 
-bool CarnetRDV::ajouterPersonne( std::string &prenom,  std::string &nom,std::string  &tel, std::string   &mail)
-{
-	listp.ajouter(prenom,nom,tel,mail);
-	return 1;
-}
-ListePersonne CarnetRDV::getlesPersonneDuRDV(std::string const &nom)
-{
-	
-}
-std::string CarnetRDV::AfficherRDV(std::string const &nom)
-{
-    ListePersonne pRDV=getlesPersonneDuRDV(nom);
-    std::string chaine="";
-    RDV *lerdv=lrdv.recherche(nom);
-    chaine=chaine+lerdv->Nom+"\n";
-    chaine+=lerdv->d.str()+"\n";
-    chaine+=lerdv->hDeb.str()+"\n";
-    chaine+=lerdv->hFin.str()+"\n";
-    chaine+= pRDV.afficherListpersonne();
-}
+
+
 
 bool CarnetRDV::EstDispo(std::string const &nom, std::string const &prenom,Date const &date,Horaire const & hDeb,Horaire const  &hFin)
 {
@@ -29,7 +11,7 @@ bool CarnetRDV::EstDispo(std::string const &nom, std::string const &prenom,Date 
 
 bool CarnetRDV::EstRDV(std::string const &nom,std::string const &prenom)
 {
-        return true;
+    return true;
 }
 
 bool CarnetRDV::supprimerPersonne(std::string const &nom,std::string const &prenom)
@@ -39,7 +21,49 @@ bool CarnetRDV::supprimerPersonne(std::string const &nom,std::string const &pren
     else
         return false;
 }
+
+bool CarnetRDV::ajouterPersonne( std::string &prenom,  std::string &nom,std::string  &tel, std::string   &mail)
+{
+    listp.ajouter(prenom,nom,tel,mail);
+    return 1;
+}
+ListePersonne CarnetRDV::getlesPersonneDuRDV(std::string const &nom)
+{
+
+}
+/*
+std::string CarnetRDV::AfficherRDV(std::string const &nom)
+{
+    ListePersonne pRDV=getlesPersonneDuRDV(nom);
+    std::string chaine="";
+    RDV *lerdv=lrdv.recherche(nom);
+    chaine=chaine+lerdv->Nom+"\n";
+    chaine+=lerdv->d.str()+"\n";
+    chaine+=lerdv->hDeb.str()+"\n";
+    chaine+=lerdv->hFin.str()+"\n";
+    chaine+= pRDV.afficherlistpersonne();
+}*/
+std::string CarnetRDV::afficherPersonne(std::string const &nom , std::string const &prenom)
+{
+    std::string chaine="";
+    Personne *lapers=listp.rechercherPersonne(nom,prenom);
+    chaine=chaine+lapers->nom+"\n";
+    chaine+=lapers->prenom+"\n";
+    chaine+=lapers->mail+"\n";
+    chaine+=lapers->tel+"\n";
+    chaine+= listp.afficherlistpersonne();
+}
+
 int main()
 {
-    return 0;
+    /*CarnetRDV c;
+    std::string nom;
+    std::string prenom;
+
+   if(c.supprimerPersonne(nom,prenom)==false)
+        std::cout<<"Suppression impossible"<<std::endl;
+   else
+        c.supprimerPersonne(nom,prenom);
+*/
+return 0;
 }
