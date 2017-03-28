41c95e8db92ea48653cf379f464c3b9a1236c32e
Nathan Baumann <nathan>
2017-03-28 21:54:18 +0200

    Ll nat

--- ListeLien.cpp
+++ ListeLien.cpp
@@ -1,3 +1,35 @@
 #include "ListeLien.h"
 
-ListeLien::ListeLien()
+ListeLien::ListeLien(): _tete{nullptr}
+{}
+
+ListeLien::~ListeLien()
+{
+	if(_tete)
+	{
+		Lien* c = _tete;
+		while(c)
+		{
+			Lien* s = c;
+			c = c->suiv;
+			delete s;
+		}
+	}
+}
+
+
+bool supprimer(Personne* p, RDV* r)
+{
+	if(_tete)
+	{
+		if(_tete->)
+
+		Lien* c = _tete;
+		while(c->suiv && (c->suiv->p != p || c->suiv->r != r))
+		{
+			c = c->suiv;
+		}
+
+		if(c)
+	}
+}
