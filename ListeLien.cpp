a6158a80d7abefb18e08bb73d021fca8d0183460
nathan-baumann <nathan-baumann@users.noreply.github.com>
2017-03-28 22:51:57 +0200

    Update ListeLien.cpp

--- ListeLien.cpp
+++ ListeLien.cpp
@@ -22,16 +22,34 @@ bool supprimer(Personne* p, RDV* r)
 {
 	if(_tete)
 	{
-		if(_tete->)
-
-		Lien* c = _tete;
-		while(c->suiv && (c->suiv->p != p || c->suiv->r != r))
+		if(_tete->p == p && _tete->r == r)
 		{
-			c = c->suiv;
+			Lien* s = _tete;
+			_tete = _tete->suiv;
+			delete s;
 		}
+		else
+		{
+			Lien* c = _tete;
+			while(c->suiv && (c->suiv->p != p || c->suiv->r != r))
+			{
+				c = c->suiv;
+			}
 
-		if(c)
+			if(c->suiv)//si c->suiv est différent de null c'est que c'est le lien à supprimer
+			{
+				Lien* s = c->suiv;
+				c->suiv = c->suiv->suiv;
+				delete s;
+			}
+			else
+				return false;//pas de correspondance
+		}
 	}
+	else
+		return false;
+
+	return true;
 }
 
 
