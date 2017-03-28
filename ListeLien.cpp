979310aad7893270837a7feea128cc78bb093159
Nathan Baumann <nathan>
2017-03-28 22:01:39 +0200

    LL maj

--- ListeLien.cpp
+++ ListeLien.cpp
@@ -1,6 +1,38 @@
 #include "ListeLien.h"
 
-ListeLien::ListeLien()
+ListeLien::ListeLien(): _tete{nullptr}
+{}
+
+ListeLien::~ListeLien()
+{
+	if(_tete)
+	{
+		Lien* c = _tete;
+		while(c)
+		{
+			Lien* s = c;
+			c = c->suiv;
+			delete s;
+		}
+	}
+}
+
+
+bool supprimer(Personne* p, RDV* r)
+{
+	if(_tete)
+	{
+		if(_tete->)
+
+		Lien* c = _tete;
+		while(c->suiv && (c->suiv->p != p || c->suiv->r != r))
+		{
+			c = c->suiv;
+		}
+
+		if(c)
+	}
+}
 
 
 bool ListeLien::ajouter(Personne *p, RDV *r)
