d1de8da024ef419afb4b724e1bf8dbb81397d63e
kilikamars <zeiger@hotmail.fr>
2017-03-31 15:12:52 +0200

    Update CarnetRDV.h

--- CarnetRDV.h
+++ CarnetRDV.h
@@ -12,13 +12,14 @@
 
 class CarnetRDV
 {
-	public:
+	private:
 		int getIDRdv(std::string nom);
 		void InsererLien(int idpers,int idrdv);
 		void supprimerRDV_BDD(std::string nom);
 		void supprimerPersonneDuRDV_BDD(std::string nomrdv,std::string nom,std::string prenom);
 		void ModifierPersonne_BDD(std::string prenom, std::string nom,std::string nouveaunom,std::string nouveauprenom,std::string tel, std::string mail);
-        void supprimerPersonne_BDD(std::string nom,std::string prenom);
+        	void supprimerPersonne_BDD(std::string nom,std::string prenom);
+	public:
 		bool creerRDV(std::string const& nom, Date const& date, Horaire const& hDeb);
 		bool creerRDV(std::string nom, Date const &date,Horaire const & hDeb,Horaire const  &hFin);
 		bool supprimerRDV(std::string const &nom );
